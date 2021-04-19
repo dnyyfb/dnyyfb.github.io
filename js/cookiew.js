a5a485837dbd8cc6bb55d5561015b3136571e62c
Dnyyfb <dnyyfb@users.noreply.github.com>
2021-04-19 20:40:49 +0800

    by 电脑菌and笔稽

--- js/cookiew.js
+++ js/cookiew.js
@@ -1,8 +1,6 @@
  var oDate = new Date();
  var lstatus = "0"
- var wstatus = "1"
  oDate.setDate(oDate.getDate() + 30);//有效期为30天
- document.cookie = "name=dnyyfb.github.io;expires=" + oDate;
- document.cookie = "name=dnyyfb.github.io;lstatus=" + lstatus;
- document.cookie = "name=dnyyfb.icoc.bz;wstatus=" + wstatus;
+ document.cookie = "name=expires" + oDate;
+ document.cookie = "name=lstatus" + lstatus;
  
