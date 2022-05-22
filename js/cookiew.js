ae9d4ad1a0e75367568165a057c028eb8494fbe9
电脑菌 <dnyyfb@qq.com>
2022-05-22 19:52:48 +0800

    修复bug by 电脑菌and笔稽

--- js/cookiew.js
+++ js/cookiew.js
@@ -1,6 +1,3 @@
- var oDate = new Date();
- var lstatus = "0"
- oDate.setDate(oDate.getDate() + 30);//有效期为30天
- document.cookie = "expires=" + oDate;
- document.cookie = "lstatus=" + lstatus;
+var lstatus = "0"
+document.cookie = "lstatus=" + lstatus;
  
